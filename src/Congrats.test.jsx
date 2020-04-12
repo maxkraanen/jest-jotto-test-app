a4212e429128f7e3a7b2c7680392a2992dcdde4d
Max Kraanen <max@maxkraanen.com>
2020-04-12 12:14:17 +1200

    added setupTests.js

--- src/Congrats.test.jsx
+++ src/Congrats.test.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
@@ -11,8 +10,6 @@ const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
   return shallow(<Congrats {...setupProps} />);
 };
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders without error", () => {
   const wrapper = setup();
